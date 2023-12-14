ff972eb10145eaff00df479dd31eb1ced0281123
agent19music <seanmotanya@gmail.com>
2023-12-14 05:28:04 +0300

    Done with assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,36 @@
 from models import Dog
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 
-def create_table(base):
+def create_table(base,engine):
+    metadata = MetaData()
+    base.metadata.create_all(bind=engine)
     pass
 
 def save(session, dog):
+    session.add(dog)
+    session.commit()
     pass
 
 def get_all(session):
+    return session.query(Dog).all()
     pass
 
 def find_by_name(session, name):
+    return session.query(Dog).filter(Dog.name == name).first()
     pass
 
 def find_by_id(session, id):
+    return session.query(Dog).filter(Dog.id == id).first()
     pass
 
 def find_by_name_and_breed(session, name, breed):
+    return session.query(Dog).filter(Dog.name == name and Dog.breed ==breed).first()
     pass
 
 def update_breed(session, dog, breed):
+    dog.breed = breed
+    return session.query(Dog).update({
+        Dog.breed: breed
+    })
     pass
